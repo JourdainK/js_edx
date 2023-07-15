f07d87ecabaf1be6043f392410efcbb4d9fd7760
JourdainK <K.jourdain@condorcet.be>
2023-07-15 18:00:53 +0200

    module 2 (start)

--- lib/php/menu.php
+++ lib/php/menu.php
@@ -1,7 +1,33 @@
-<ul class="navbar navbar-expand- fixed-top l navbar-primary bg-dark p-2">
-    <li><a href="index.php?page=welcome.php">Home</a></li>
-    <li><a href="index.php?page=page02.php">page02</a></li>
-    <li><a href="index.php?page=page03.php">page03</a></li>
-    <li><a href="index.php?page=page04.php">page04</a></li>
-    <li><a href="index.php?page=page05.php">page05</a></li>
-</ul>
+<nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container-fluid text-light">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav text-light">
+                <li class="nav-item">
+                    <a class="nav-link active text-white" aria-current="page" href="index.php?page=welcome.php">Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Module 1
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="index.php?page=page02.php">external lib</a></li>
+                        <li><a class="dropdown-item" href="index.php?page=page03.php">function plotter</a></li>
+                        <li><a class="dropdown-item" href="index.php?page=page04.php">variables</a></li>
+                        <li><a class="dropdown-item" href="index.php?page=page05.php">object and functions</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Module 2
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="index.php?page=mod02_01.php">loop, operators</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ pages/mod02_01.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<div class="row">
+
+    <div class="col-6 text-center align-middle">
+        <h3>--</h3>
+        <p>stuff</p>
+    </div>
+
+    <div class="col-6 text-center align-middle">
+        <h3>--</h3>
+        <p>Stuff</p>
+    </div>
+</div>
